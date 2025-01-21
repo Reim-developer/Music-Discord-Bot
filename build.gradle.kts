9d109785689bf663560dfc818061845cc3310967
Reim-developer <contact.kaxtr@gmail.com>
2025-01-21 21:53:05 +0700

    Added new build script file

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+/*
+ * Project: Music Discord Bot
+ * Contribution: Reim
+ * File: build.gradle.kts
+ */
+
+plugins {
+    kotlin("jvm") version "2.1.0"
+}
+
+group = "MusicDiscordBot"
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+    implementation("dev.kord:kord-core:0.15.0")
+    implementation("org.slf4j:slf4j-api:2.0.16")
+    implementation("ch.qos.logback:logback-classic:1.5.16")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
