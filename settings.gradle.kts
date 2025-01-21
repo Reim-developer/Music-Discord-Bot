55a4b1f001d82e2b722e47389fdd1ceee1e7dda3
Reim-developer <contact.kaxtr@gmail.com>
2025-01-21 21:53:37 +0700

    Added new settings script file

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+/*
+ * Project: Music Discord Bot
+ * Contribution: Reim
+ * File: settings.gradle.kts
+ */
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+    }
+    plugins {
+        id("org.jetbrains.kotlin.jvm") version "1.8.10"
+    }
+}
+
+rootProject.name = "MusicDiscordBot"
