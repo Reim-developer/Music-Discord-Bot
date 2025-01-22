a0f041a74549458c888ee5791f0fd1efbfc73922
Reim-developer <contact.kaxtr@gmail.com>
2025-01-22 22:55:44 +0700

    Fixed and removed deprecated method + Added new class, function to sync/load all slash commands

--- src/main/java/discord/Bot.java
+++ src/main/java/discord/Bot.java
@@ -7,7 +7,9 @@
 
 package discord;
 
+import discord.command.Core;
 import discord.command.music.Join;
+import discord.command.music.Leave;
 import discord.ulti.Token;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -18,7 +20,8 @@ import java.util.EnumSet;
 public class Bot {
 
     private final Token token = new Token();
-    private final Join join = new Join(this);
+    private final Core botCore = new Core(this);
+
     public JDA jda;
 
     public static void main(String[] args) {
@@ -34,11 +37,12 @@ public class Bot {
         try {
             bot.jda = JDABuilder.create(botToken, botIntents)
                     .setActivity(Activity.watching("Reim developer"))
-                    .addEventListeners(new Join(bot))
+                    .addEventListeners(new Join())
+                    .addEventListeners(new Leave())
                     .build()
                     .awaitReady();
 
-            bot.join.setJoinCommand();
+            bot.botCore.syncCommands();
         } catch (InterruptedException interruptedException) {
             System.out.println("Interrupted");
         }
