ab219715064a79d335a384600b562aef4495a70b
Reim-developer <contact.kaxtr@gmail.com>
2025-01-22 22:56:28 +0700

    Added new slash command: + Leave

---
+++ src/main/java/discord/command/music/Leave.java
@@ -0,0 +1,47 @@
+/*
+ * Project: Music Discord Bot
+ * Contribution: Reim
+ * Package: discord.music
+ * File: Left.java
+ */
+
+package discord.command.music;
+
+import discord.ulti.GuildUlti;
+import discord.ulti.PermUlti;
+import discord.ulti.UserUlti;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.managers.AudioManager;
+import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public class Leave extends ListenerAdapter {
+    private final GuildUlti guildUlti = new GuildUlti();
+    private final PermUlti permUlti = new PermUlti();
+    private final UserUlti userUlti = new UserUlti();
+
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        if(event.getName().equals("leave")) {
+            guildUlti.checkIfExists(event.getGuild(), event);
+            permUlti.checkVoicePerm(event.getGuild(), event);
+
+            if(!userUlti.isBotVoice(event.getGuild(), event)) {
+                event.reply("The bot is not in any voice channel, so there's no need to leave").queue();
+                return;
+            }
+            if(!userUlti.isVoice(event.getMember(), event)) return;
+
+            AudioManager audioManager = Objects.requireNonNull(event.getGuild()).getAudioManager();
+            ReplyCallbackAction replyCallbackAction = event.deferReply();
+
+            System.out.println(replyCallbackAction);
+            audioManager.closeAudioConnection();
+
+            event.reply("Successfully leave voice channel").queue();
+        }
+    }
+}
