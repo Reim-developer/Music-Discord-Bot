79e259ecd804d98306fc7174602190ac8906c2b7
Reim-developer <contact.kaxtr@gmail.com>
2025-01-22 18:30:28 +0700

    Removed unused dependencies and add new new dependencies Added new sourceSet

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,9 +16,13 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
-    implementation("dev.kord:kord-core:0.15.0")
     implementation("org.slf4j:slf4j-api:2.0.16")
     implementation("ch.qos.logback:logback-classic:1.5.16")
+    implementation("net.dv8tion:JDA:5.2.1")
+}
+
+sourceSets {
+    main { kotlin.srcDirs("src/main/kotlin") }
 }
 
 kotlin {
